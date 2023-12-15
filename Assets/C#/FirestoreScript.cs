899cb7e4afbfe3c2e8002d12a8b3bb7ca31e338e
Muto Sota <hasu_m.s@icloud.com>
2023-12-16 02:45:40 +0900

    ボタンでfirestoreにデータ送るとこまで

--- Assets/C#/FirestoreScript.cs
+++ Assets/C#/FirestoreScript.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 
 public class FirestoreScript : MonoBehaviour
 {
-    public void Start()
+    // キャプチャされた画像データ
+    byte[] dataImage;
+
+    public void sendWindow()
     {
         var db = FirebaseFirestore.DefaultInstance;
-        DocumentReference docRef = db.Collection("cities").Document("LA");
+        DocumentReference docRef = db.Collection("windowList").Document("myWindow");
         Dictionary<string, object> city = new Dictionary<string, object>
         {
                 { "Name", "Los Angeles" },
@@ -17,7 +20,29 @@ public class FirestoreScript : MonoBehaviour
                 { "Country", "USA" }
         };
         docRef.SetAsync(city).ContinueWithOnMainThread(task => {
-            Debug.Log("Added data to the LA document in the cities collection.");
+            Debug.Log("Added data.");
         });
+
+        StartCoroutine(DoScreenShot());
+    }
+
+    IEnumerator DoScreenShot()
+    {
+        Debug.Log($"DoScreenShot");
+ 
+        // レンダリング後に処理を開始
+        yield return new WaitForEndOfFrame();
+ 
+        // Texture2D でスクリーンショットを取得
+        Texture2D textureScreenCapture = ScreenCapture.CaptureScreenshotAsTexture();
+ 
+        // EncodeToJPG で JPEG に変換する。EncodeToPNG なら PNG に変換できる
+        dataImage = textureScreenCapture.EncodeToJPG();
+ 
+        Debug.Log($"撮影完了");
+        Debug.Log($"{dataImage}");
+ 
+        // textureScreenCapture を削除
+        UnityEngine.Object.Destroy(textureScreenCapture);
     }
 }
