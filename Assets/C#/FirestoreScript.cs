5f94d938559316292a47d6ecdfea5751fd68b4e4
Muto Sota <hasu_m.s@icloud.com>
2023-12-16 03:53:56 +0900

    firestore/storageに画像保存できるようになった

--- Assets/C#/FirestoreScript.cs
+++ Assets/C#/FirestoreScript.cs
@@ -1,16 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Firebase.Extensions;
 using Firebase.Firestore;
+using Firebase.Storage;
 using UnityEngine;
 
 public class FirestoreScript : MonoBehaviour
 {
     // キャプチャされた画像データ
-    byte[] dataImage;
+    byte[] dataImage = new byte[]{};
+    public GameObject canvas;
 
     public void sendWindow()
     {
+        FirebaseStorage storage = FirebaseStorage.DefaultInstance;
+        StorageReference storageRef = storage.GetReferenceFromUrl("gs://ice-window.appspot.com");
+
+        StartCoroutine(DoScreenShot());
+
+        StorageReference windowRef = storageRef.Child("myWindow.jpg");
+        windowRef.PutBytesAsync(dataImage).ContinueWith((Task<StorageMetadata> task) => {
+            if (task.IsFaulted || task.IsCanceled) {
+                Debug.Log(task.Exception.ToString());
+            }
+            else {
+                StorageMetadata metadata = task.Result;
+                string md5Hash = metadata.Md5Hash;
+                Debug.Log("Finished uploading...");
+                Debug.Log("md5 hash = " + md5Hash);
+            }
+        });
+
         var db = FirebaseFirestore.DefaultInstance;
         DocumentReference docRef = db.Collection("windowList").Document("myWindow");
         Dictionary<string, object> city = new Dictionary<string, object>
@@ -22,13 +43,13 @@ public class FirestoreScript : MonoBehaviour
         docRef.SetAsync(city).ContinueWithOnMainThread(task => {
             Debug.Log("Added data.");
         });
-
-        StartCoroutine(DoScreenShot());
     }
 
     IEnumerator DoScreenShot()
     {
         Debug.Log($"DoScreenShot");
+
+        canvas.SetActive(false);
  
         // レンダリング後に処理を開始
         yield return new WaitForEndOfFrame();
@@ -40,9 +61,10 @@ public class FirestoreScript : MonoBehaviour
         dataImage = textureScreenCapture.EncodeToJPG();
  
         Debug.Log($"撮影完了");
-        Debug.Log($"{dataImage}");
  
         // textureScreenCapture を削除
         UnityEngine.Object.Destroy(textureScreenCapture);
+
+        canvas.SetActive(true);
     }
 }
