d9ae41c559a06393a4b1325f0a2d465aac2ecfbd
Muto Sota <hasu_m.s@icloud.com>
2023-12-16 20:48:12 +0900

    招待する人のIDを入力したら下に出てくるようにした

--- Assets/C#/UserAdd.cs
+++ Assets/C#/UserAdd.cs
@@ -7,16 +7,17 @@ using TMPro;
 public class UserAdd : MonoBehaviour
 {
     
-    public Text UserId;
+    public GameObject UserId;
+    public GameObject inputField;
     //出力用のテキスト
 
     //inputFieldのOnEndEditに設定する用の関数
     public void UserIdLine()
     {
         //InputFieldコンポーネントのtextを変数に代入
-        string inputFieldText = GetComponent<InputField>().text;
+        string inputFieldText = inputField.GetComponent<Text>().text;
         //出力用のテキストに代入
-        UserId.text = inputFieldText;
+        UserId.GetComponent<Text>().text = inputFieldText;
     }
     
 }
